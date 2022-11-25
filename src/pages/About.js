62c13cc19b817d838abbbbd112fbe2630f234070
Ilie1985 <ilie.marian32@yahoo.com>
2022-11-25 16:07:36 +0000

    about us

--- src/pages/About.js
+++ src/pages/About.js
@@ -5,13 +5,7 @@ const About = () => {
     <section className="section about-section">
       <h1 className="section-title">about us</h1>
       <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita unde
-        doloremque, dolorum rem sed qui consequuntur, distinctio provident quod,
-        quam autem blanditiis facere minima? Quas repudiandae doloremque nemo
-        cupiditate sequi. Perferendis consectetur dolor exercitationem nisi
-        reiciendis officiis ex, aliquam, tenetur nemo nobis ad quas, illo in
-        ratione perspiciatis fuga ipsa ullam vel quis? Cum, facilis inventore
-        aliquid nisi voluptatibus quae!
+      Expertly crafted using only natural, fresh and vegan friendly ingredients, alongside crafted spirits, and our own artisan cordials and liqueurs.
       </p>
     </section>
   );
