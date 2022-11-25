dacddd5b43d17ef3b875d411d1e2766308669553
Ilie1985 <ilie.marian32@yahoo.com>
2022-11-25 16:21:33 +0000

    about us updated

--- src/pages/About.js
+++ src/pages/About.js
@@ -5,7 +5,7 @@ const About = () => {
     <section className="section about-section">
       <h1 className="section-title">about us</h1>
       <p>
-      Expertly crafted using only natural, fresh and vegan friendly ingredients, alongside crafted spirits, and our own artisan cordials and liqueurs.
+      We are expertly crafted using only natural, fresh and vegan friendly ingredients, alongside crafted spirits, and our own artisan cordials and liqueurs.
       </p>
     </section>
   );
